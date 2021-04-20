efb37d51f253f96819d08a2da0ef60e7e1e31633
Asghar Nazir <asghar@syntaxtech.com>
2021-04-20 03:06:16 -0400

    Repls till 182

---
+++ src/Repl_168/Repl_168.java
@@ -0,0 +1,14 @@
+package Repl_168;
+
+class Main {
+     String myString="hello";
+
+    public final String reverseMyString(){
+        return new StringBuilder(myString).reverse().toString();
+    }
+
+    public static void main(String[] args){
+        Main main=new Main();
+        System.out.println(main.reverseMyString());
+    }
+}
---
+++ src/Repl_174/MyInterface.java
@@ -0,0 +1,6 @@
+package Repl_174;
+
+interface MyInterface{
+    void method1();
+    void method2();
+}
